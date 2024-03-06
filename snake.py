1d70e1ba8b36fb62a200b13e6ef46f72a9e690a1
Ricardo <ricarditomontserrat@gmail.com>
2024-03-06 21:10:17 +0100

    Functional snake game

--- snake.py
+++ snake.py
@@ -5,13 +5,13 @@ pygame.init()
 run = True
 
 RESOLUTION = 2
-FPS = 60
+FPS = 20
 
 WIDTH, HEIGHT = 320 * RESOLUTION, 240 * RESOLUTION
 RES_FACTOR = WIDTH // 320
 
-BLOCK_SIZE = 20 * RES_FACTOR
-INITIAL_SPEED = 10
+BLOCK_SIZE = 10 * RES_FACTOR
+INITIAL_SPEED = 1
 INITIAL_DIRECTION = [0, -1]
 
 
@@ -27,17 +27,15 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 x, y = WIDTH // 2, HEIGHT // 2
 head, rect_color = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE), (255, 0, 0)
-direction, spd = INITIAL_DIRECTION, INITIAL_SPEED
+direction = INITIAL_DIRECTION
 
 fruitx, fruity, fruitcolor = get_random_coords()
 score = 0
 
-tails = []
+tails = [pygame.Rect(x, y + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE),
+         pygame.Rect(x, y + 2 * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)]
 clock = pygame.time.Clock()
 while run:
-    dt = clock.tick(FPS) * 0.001
-    screen.fill((0, 0, 0))
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -53,36 +51,41 @@ while run:
             elif event.key == pygame.K_RIGHT:
                 direction = [1, 0]
 
+    screen.fill((0, 0, 0))
+
     fruit = pygame.Rect(fruitx, fruity, BLOCK_SIZE, BLOCK_SIZE)
     pygame.draw.rect(screen, fruitcolor, fruit)
 
+    tailbefore = head.copy()
+    head.x += direction[0] * BLOCK_SIZE
+    head.y += direction[1] * BLOCK_SIZE
     pygame.draw.rect(screen, rect_color, head)
-    movex = round(spd * direction[0] * BLOCK_SIZE * dt)
-    movey = round(spd * direction[1] * BLOCK_SIZE * dt)
-    head.move_ip(movex, movey)
-
-    x, y = head.x, head.y
-    if x < 0 or x + BLOCK_SIZE > WIDTH:
-        head.x -= movex
-    if y < 0 or y + BLOCK_SIZE > HEIGHT:
-        head.y -= movey
+    for tail in tails:
+        tmp = tail.copy()
+        tail.x, tail.y = tailbefore.x, tailbefore.y
+        pygame.draw.rect(screen, rect_color, tail)
+        tailbefore = tmp
 
     if head.colliderect(fruit):
         fruitx, fruity, fruitcolor = get_random_coords()
-        spd += 1
-        # spd = 0
         score += 1
 
-        tail = pygame.Rect(x - direction[0] * BLOCK_SIZE,
-                           y - direction[1] * BLOCK_SIZE,
+        last_tail = tails[-1]
+        tail = pygame.Rect(last_tail.x + BLOCK_SIZE,
+                           last_tail.y + BLOCK_SIZE,
                            BLOCK_SIZE, BLOCK_SIZE)
         tails.append(tail)
 
+    x, y = head.x, head.y
+    if x < 0 or x + BLOCK_SIZE > WIDTH or y < 0 or y + BLOCK_SIZE > HEIGHT:
+        run = False
+    elif head in tails:
+        run = False
+
     text = font.render(f"Score: {score}", True, (255, 255, 255))
     screen.blit(text, (10, 10))
-    text = font.render(f"x: {x}, y: {y}", True, (255, 255, 255))
-    screen.blit(text, (10, 50))
 
+    clock.tick(FPS)
     pygame.display.update()
 
 pygame.quit()
